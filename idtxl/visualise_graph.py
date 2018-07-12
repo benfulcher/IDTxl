24a787f9ea2cf00e7fb78b051c68b9cec601ee3a
Patricia Wollstadt <patricia.wollstadt@gmx.de>
2018-07-12 21:55:55 +0200

    Remove plt.show() from plotting functions
    Remove plt.show() from plotting functions. The show() command has to
    be called externally. Makes unit testing and batch processing easier
    (e.g., generated functions can be saved directly without needing to
    close every figure).

--- idtxl/visualise_graph.py
+++ idtxl/visualise_graph.py
@@ -69,8 +69,6 @@ def plot_network(results, weights, fdr=True):
     _plot_adj_matrix(
         results.get_adjacency_matrix(weights, fdr),
         cbar_label=weights)
-    plt.show()
-
     return graph, fig
 
 
@@ -144,7 +142,6 @@ def plot_selected_vars(results, target, sign_sources=True,
 
     plt.plot([-0.5, max_lag + 0.5], [0.5, 0.5],
              linestyle='--', linewidth=1, color='0.5')
-    plt.show()
     return graph, fig
 
 
@@ -215,6 +212,10 @@ def plot_mute_graph():
         0 -> 3, u = 2 (non-linear)
         3 -> 4, u = 1
         4 -> 3, u = 1
+
+    Returns:
+        Figure
+            figure handle, Figure object from the matplotlib package
     """
     graph = nx.DiGraph()
     graph.add_nodes_from(np.arange(5))
@@ -228,15 +229,15 @@ def plot_mute_graph():
         3: np.array([2, 1]),
         4: np.array([3, 1]),
     }
-    plt.figure()
+    fig = plt.figure()
     nx.draw(graph, pos=pos, with_labels=True, node_size=900, alpha=1.0,
             node_color='cadetblue', font_weight='bold',
             edge_color=['r', 'k', 'r', 'k', 'k'], hold=True)
     nx.draw_networkx_edge_labels(graph, pos=pos)
     plt.text(2, 0.1, 'non-linear interaction in red')
-    plt.show()
     # see here for an example on how to plot edge labels:
     # http://stackoverflow.com/questions/10104700/how-to-set-networkx-edge-labels-offset-to-avoid-label-overlap
+    return fig
 
 
 def plot_network_comparison(results, weights, fdr=True):
@@ -285,6 +286,4 @@ def plot_network_comparison(results, weights, fdr=True):
     _plot_adj_matrix(results.get_adjacency_matrix('pvalue'), mat_color='gray',
                      cbar_label='p-value', cbar_stepsize=0.05)
     ax.set_title('p-value [%]', y=1.1)
-    plt.show()
-
     return graph_union, fig
